7b06d70e2ce2e9a9cd3e0cbcb71c83875b0728bf
Lev Roman <leva_6@mail.ru>
2020-11-10 21:04:07 +0200

    Added service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotosService } from './services/photos.service'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs'
+import { catchError, tap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontForExif';
+export class AppComponent implements OnInit {
+
+  result: String = ''
+  private apiUrl = 'localhost:8080/form'
+  constructor(private http: HttpClient, private photosService: PhotosService){}
+
+  ngOnInit() {
+  }
+
+  processPhoto(): Observable<any> {
+    return this.http.post(this.apiUrl, {}, {})
+      .pipe(
+        tap(data => {
+          console.log(data)
+        }),
+        catchError((err) => {
+          this.result = 'Error'
+          console.log('Error' + err)
+        })
+      );
+  }
 }
